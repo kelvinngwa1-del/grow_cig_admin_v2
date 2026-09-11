29f2e3c312d30d1411cd487b10c29101992f1c52
Kelvin Ngwa <growcig@gmail.com>
2026-09-11 07:57:00 +0400

    Add loan agreement workflow and link agreements to loan details

--- components/loans/loan-details.tsx
+++ components/loans/loan-details.tsx
@@ -1028,6 +1028,7 @@ export default function LoanDetails({
               </p>
 
               {[
+                "pending",
                 "approved",
                 "active",
                 "disbursed",
